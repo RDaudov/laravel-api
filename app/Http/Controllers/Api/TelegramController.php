59dfaa518f30497b855c5b3fa08ad4d3ce1a88e2
RDaudov <96396811+RDaudov@users.noreply.github.com>
2025-02-26 23:18:14 +0300

    add tg controller

---
+++ app/Http/Controllers/Api/TelegramController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class TelegramController extends Controller
+{
+    private $botToken;
+
+    public function __construct()
+    {
+        $this->botToken = env('TG_BOT_TOKEN');
+    }
+
+    public function handleWebhook(Request $request)
+    {
+        $data = $request->all();
+
+        if (isset($data['message'])) {
+            $chatId = $data['message']['chat']['id'];
+            $text = $data['message']['text'];
+
+            if ($text === '/start') {
+                $this->sendMessage($chatId, 'Привет! Я ваш бот. Используйте /help для списка команд.');
+            } elseif ($text === '/help') {
+                $this->sendMessage($chatId, 'Доступные команды: /start, /help');
+            } else {
+                $this->sendMessage($chatId, 'Я не понимаю эту команду.');
+            }
+        }
+
+        return response('OK', 200);
+    }
+
+    private function sendMessage($chatId, $text)
+    {
+        $url = "https://api.telegram.org/bot" . $this->botToken . "/sendMessage";
+        $data = [
+            'chat_id' => $chatId,
+            'text' => $text,
+        ];
+
+        Http::post($url, $data);
+    }
+}
